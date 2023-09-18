98a2d08e10bae3297b414fd3a6bb7b9232187b87
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-18 14:48:50 -0600

    practice complete.

--- Modifiers_Abstract_OverridesReview/Program.cs
+++ Modifiers_Abstract_OverridesReview/Program.cs
@@ -1,11 +1,53 @@
-﻿namespace Modifiers_Abstract_OverridesReview
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿namespace Modifiers_Abstract_OverridesReview;
 
-            Console.ReadLine();
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Monster> monsters = new List<Monster>();
+        monsters.Add(new Charmander { Name = "Charmander", Type = "Fire", Power = 4, MoveList = { "Ember", "Fire Blast" } });
+        monsters.Add(new Bulbasaur { Name = "Bulbasaur", Type = "Grass", Power = 2, MoveList = { "Vine Whip", "Solar Beam"} });
+        monsters.Add(new Squirtle { Name = "Squirtle", Type = "Water", Power = 3, MoveList = { "Water Gun", "Hydro Pump" } });
+
+        foreach (Monster monster in monsters)
+        {
+            Console.WriteLine(monster.ToString());
         }
+
+        Console.ReadLine();
     }
+}
+
+public interface IStats
+{
+    string Name { get; set; }
+    string Type { get; set; }
+    int Power { get; set; }
+}
+
+public abstract class Monster : IStats
+{
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public int Power { get; set; }
+    public abstract List<string> MoveList { get; set; }
+    public override string ToString()
+    {
+        return $"Name is {Name}.\tType is {Type}.\tPower level: {Power}.\tIt's moveset: {MoveList}";
+    }
+}
+
+public class Charmander : Monster
+{
+    public override List<string> MoveList { get; set; } = new List<string>(2);
+}
+
+public class Bulbasaur : Monster
+{
+    public override List<string> MoveList { get; set; } = new List<string>(2);
+}
+
+public class Squirtle : Monster
+{
+    public override List<string> MoveList { get; set; } = new List<string>(2);
 }
